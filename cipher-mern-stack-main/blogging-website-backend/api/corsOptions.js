c1a7d4b28b3fa1106bf8400efaf1febe62333cab
amaana05 <164677417+amaana05@users.noreply.github.com>
2024-07-15 17:35:51 +0530

    Initial commit

---
+++ cipher-mern-stack-main/blogging-website-backend/api/corsOptions.js
@@ -0,0 +1,17 @@
+
+const allowedOrigins = require('../config/allowedOrigins');
+
+const corsOptions = {
+    origin: (origin, callback) => {
+
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            callback(null, true);
+        } else{
+            callback(new Error('Not alowed by CORS.'));
+        }
+    },
+    credentials: true,      //these could be coookie, http auth. its basically requests from dif dif domains
+    optionsSuccessStatus: 200   //use of CORS prefligh check
+};
+
+module.exports = corsOptions;
